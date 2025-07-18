99695872cc9ebaab1e30f2298bfc0d591dfdb503
hurlx <n247684@gmail.com>
2025-07-18 04:50:40 +0300

    Update page.jsx

--- app/Bag/page.jsx
+++ app/Bag/page.jsx
@@ -4,8 +4,7 @@ import ProductGrid from "@/components/ProductGrid";
 
 export default function BagGallery() {
   return (
-     <main className="bg-[#f5f5f5] min-h-screen py-10">
-      <h1 className="text-3xl font-bold text-center mb-8">Featured Bags</h1>
+     <main className="min-h-screen mt-7 py-10">
       <ProductGrid />
     </main>
   );
